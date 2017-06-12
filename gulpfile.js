72f86e23afee52bcbbcba58c7236459114e9096a
vshes <vshes.60@gmail.com>
2017-06-12 13:09:34 -0500

    (feat) search and date features

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     clean = require('gulp-clean'),
     inject = require('gulp-inject'),
     bowerFiles =require('main-bower-files'),
-    browserSync =require('browser-sync').create();
+    browserSync =require('browser-sync').create(),
+    sortFiles = require('gulp-angular-filesort');
 
 var config ={
     paths:{
@@ -24,12 +25,12 @@ gulp.task('inject',function () {
         {read: false});
 
     var jsFiles = gulp.src([config.paths.src + '/**/*.js'],
-        {read:false});
+        {read:true});
 
       return  gulp.src(config.paths.src + '/index.html')
         .pipe(inject(gulp.src(bowerFiles(),{read:false}),{name: 'bower'},{addRootSlash:false}))
         .pipe(inject(cssFiles,{ignorePath :'/src', addRootSlash:false}))
-        .pipe(inject(jsFiles,{ignorePath: '/src',addRootSlash:false}))
+        .pipe(inject(jsFiles.pipe(sortFiles()),{ignorePath: '/src',addRootSlash:false}))
         .pipe(gulp.dest(config.paths.src));
 });
 
@@ -46,5 +47,4 @@ gulp.task('serve',['inject'],function () {
        ]
    });
 });
-
 
